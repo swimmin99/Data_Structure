29391318d6c02dc05b6b7289241cdf7d1bf71f4b
HANGHAE <109887066+swimmin99@users.noreply.github.com>
2022-11-29 15:30:54 +0900

    Add files via upload

---
+++ list2.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define IS_EMPTY(first) (!(first))
+#define MALLOC(p,s) if (!((p) = malloc(s))) { fprintf(stderr, "Insufficient memory"); exit(EXIT_FAILURE);}
+
+typedef struct listNode* listPointer;
+typedef struct listNode {
+	int data;
+	listPointer link;
+} listNode;
+
+
+void delete2(listPointer *first, listPointer trail, listPointer x) {
+	if (trail)
+		trail->link = x->link;
+	else
+		(*first) = (*first)->link;
+	free(x);
+}
+
+
+void insert(listPointer *first, int n) {
+
+	listPointer temp;
+	MALLOC(temp, sizeof(*temp));
+	temp->data = n;
+	listPointer search, trail;
+	trail = NULL;
+	int flag = 1;
+
+	for (search = (*first); ; search = search->link) {
+		if (!search) {
+			break;
+		}
+		else {
+			if (search->data > n) {
+				break;
+			}
+			else if (search->data == n) {
+				delete2(first, trail, search);
+				break;
+			}
+		}
+		trail = search;
+	}
+
+
+	if (trail) {
+		temp->link = trail->link;
+		trail->link = temp;
+	}
+	else {
+		temp->link = (*first);
+		(*first) = temp;
+	}
+}
+
+void printList(listPointer first) {
+	printf("The list contatins: ");
+	listPointer search = first;
+
+	for (; search; search = search->link) {
+		printf("%d ", search->data);
+	}
+	printf("\n");
+}
+
+
+int main() {
+	listPointer first = NULL;
+	//======================= FILE OPEN ====================//
+	char filename[100];
+	printf("Input filename : ");
+	gets(filename);
+	FILE* fp = NULL;
+	if (!(fp = fopen(filename, "r+"))) {
+		fprintf(stderr, "File open failed");
+		exit(EXIT_FAILURE);
+	}
+
+	//====================== FILE READ ======================//
+	int n1;
+
+	while (!feof(fp)) {
+		int n;
+		fscanf(fp, "%d", &n);
+		insert(&first, n);
+		printList(first);
+	}
+
+	//====================== INSERT ==========================//
+	int input;
+
+	while (1) {
+		scanf("%d", &input);
+		if (input == -1)
+			break;
+		insert(&first, input);
+		printList(first);
+	}
+
+	//====================== FREE ==========================//
+	while (first != NULL) {
+		delete2(&first, NULL, NULL);
+	}
+
+	fclose(fp);
+	return 0;
+}
